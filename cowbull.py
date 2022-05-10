65c3e7e52d5b5ea26616be20831755245c893314
sradhanjali79 <101653631+sradhanjali79@users.noreply.github.com>
2022-05-10 16:46:13 +0530

    Add files via upload

---
+++ cowbull.py
@@ -0,0 +1,40 @@
+import random
+
+def cowbull():
+    list1=[0,1,2,3,4,5,6,7,8,9]
+    list2=[]
+    for i in range(5):
+        x=random.choice(list1)
+        # if x not in list2:
+        list2.append(x)
+    # print(list2)
+    bull=[]
+    cow=[]
+    for i in range(10):
+        x=int(input("enter any num..."))
+        y=int(input("enter position..."))
+        if x in list2:
+            p=list2.index(x)
+            if y==p:
+                bull.insert(y,x)
+                print("bull",bull)
+            else:
+                cow.append(x)
+                print("cow ",cow)
+        else:
+            print("sorry this no. is not exist")
+    if bull==list2:
+        print("congratulation",user,"you are the winner")
+    else:
+        print("opps",user,"you are the looser")
+    again=input("Do u want to play again...")
+    if again=="yes":
+        cowbull()
+    else:
+        print("Bye")
+    
+user=input("enter your name...")
+print("  welcome",user,"  ")
+print("[0,1,2,3,4,5,6,7,8,9]")
+cowbull()
+
